b84a17c1d9fa628c31eb2974a37129a81d6b74f3
PRAJAKTA-PRIYADARSHINI <prajaktapriyadarshinibehera@gmail.com>
2024-12-08 01:49:24 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+</head>
+<style>
+    footer{
+        background-color:#1c4966;
+        text-align: center;
+    }
+</style>
+<body>
+    <footer>
+        <h3>Newspaper2024<i class="fa-regular fa-copyright"></i></h3>
+    </footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
+</head>
+<body>
+    <header>
+<div class="text-center mt-3">
+       <h2 >NEWSPAPER  <i class="fa-solid fa-newspaper"></i></h2>
+    </div>
+</header>
+    
+</body>
+</html>
